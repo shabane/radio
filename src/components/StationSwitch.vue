73282cf4f9f69e75b7c511febe3ead0d41403792
shabane <m.mohamadshabane@gmail.com>
2025-01-21 03:37:34 +0330

    feat: create a station switch:
    now the statin switch will fix to bottom
    
    silly me forgot to `git add` this file

---
+++ src/components/StationSwitch.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="slidecontainer">
+      <input
+        type="range"
+        min="1"
+        max="100"
+        value="50"
+        class="slider"
+        id="stationSlider"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StationSwitch",
+};
+</script>
+
+<style scoped>
+.slidecontainer {
+  width: 100%;
+}
+
+.slider {
+  -webkit-appearance: none;
+  width: 90%;
+  height: 15px;
+  border-radius: 5px;
+  background: #313131;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: 0.2s;
+  transition: opacity 0.2s;
+}
+
+.slider:hover {
+  opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background: #2874a6;
+  cursor: pointer;
+  opacity: 1;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #04aa6d;
+  cursor: pointer;
+}
+</style>
