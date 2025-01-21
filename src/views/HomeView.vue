7be69cac19ab7589ec0dc27d00abdec7643fe9ab
shabane <m.mohamadshabane@gmail.com>
2025-01-21 04:06:44 +0330

    chore: add volume icon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <div class="col">
       <StationSwitch class="stationSwitch col-10" />
       <div class="fix-btm-vol col-2">
+        <span class="icono-volume"></span>
         <button class="btn btn-outline-secondary">0</button>
         <button class="btn btn-outline-secondary m-1">50</button>
         <button class="btn btn-outline-light">100</button>
