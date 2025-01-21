282c55b9dddbab368bd43854e67474b5ae160ad2
shabane <m.mohamadshabane@gmail.com>
2025-01-21 04:36:47 +0330

    feat: @click will change the selection of vol btn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,9 +4,29 @@
       <StationSwitch class="stationSwitch col-10" />
       <div class="fix-btm-vol col-2">
         <span class="icono-volume"></span>
-        <button class="btn btn-outline-secondary">0</button>
-        <button class="btn btn-outline-secondary m-1">50</button>
-        <button class="btn btn-outline-light">100</button>
+        <button
+          class="btn btn-outline-secondary"
+          @click="changeVolTo(0)"
+          id="vol-0"
+        >
+          I
+        </button>
+
+        <button
+          class="btn btn-outline-secondary m-1"
+          @click="changeVolTo(50)"
+          id="vol-50"
+        >
+          II
+        </button>
+
+        <button
+          class="btn btn-outline-light btn-outline-secondary"
+          @click="changeVolTo(100)"
+          id="vol-100"
+        >
+          III
+        </button>
       </div>
     </div>
   </div>
@@ -17,5 +37,20 @@ import StationSwitch from "@/components/StationSwitch";
 export default {
   name: "HomeView",
   components: { StationSwitch },
+  data() {
+    return {
+      volume: 100,
+    };
+  },
+  methods: {
+    changeVolTo(vol) {
+      for (let i of [0, 50, 100]) {
+        document
+          .getElementById(`vol-${i}`)
+          .classList.remove("btn-outline-light");
+      }
+      document.getElementById(`vol-${vol}`).classList.add("btn-outline-light");
+    },
+  },
 };
 </script>
