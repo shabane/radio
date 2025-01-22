60fc25863abda45375b499ad0cea3b92e92303cf
shabane <m.mohamadshabane@gmail.com>
2025-01-23 02:28:09 +0330

    chore: make babel recognize private variables

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: [
+    "@babel/plugin-transform-class-properties",
+    "@babel/plugin-transform-private-methods",
+  ],
 };
