9bcb898f653ac54c9bc9386b26c8d2d14b5aac1d
shabane <m.mohamadshabane@gmail.com>
2025-01-23 22:03:05 +0330

    feat: fn -> get all stations:
    creat fn to list all stations,
    
    remove default song src from player,

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="home">
     <audio controls autoplay id="player" hidden>
-      <source
-        src="https://raw.githubusercontent.com/bit-orbit/lofi/master/files/musics/nuver-exhale.opus"
-        type="audio/ogg"
-        autoplay
-      />
+      <source src="" type="audio/ogg" autoplay />
     </audio>
     <div class="col">
-      <StationSwitch class="stationSwitch col-10" id="stationSwitch" />
+      <StationSwitch
+        class="stationSwitch col-10"
+        id="stationSwitch"
+        station-min="0"
+        v-if="stations"
+        :station-max="stations.length - 1"
+      />
       <div class="fix-btm-vol col-2">
         <span class="icono-volume"></span>
         <button
@@ -82,12 +84,19 @@ export default {
       document.getElementById(`vol-${vol}`).classList.add("btn-outline-light");
       document.getElementById("player").volume = vol / 100;
     },
+
+    getStations() {
+      let radio = new Radio();
+      radio.listStation().then((stations) => {
+        this.stations = stations;
+      });
+    },
   },
   mounted() {
     let radio = new Radio();
     radio.listStation().then((station_list) => {
       this.stations = station_list;
-      station_list[rnd(station_list.length - 1)].listMusic().then((musics) => {
+      station_list[rnd(station_list.length)].listMusic().then((musics) => {
         this.musics = musics;
         this.current_music = musics[rnd(musics.length - 1)];
         document.getElementById("player").src = this.current_music.url;
