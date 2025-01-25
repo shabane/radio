e746527f2e114cf90a4392d461e1fd1b8de05cf1
shabane <m.mohamadshabane@gmail.com>
2025-01-26 02:58:35 +0330

    feat: fn to get and set volume on cookies and:
    and lint setCookie() function

--- src/tools.js
+++ src/tools.js
@@ -5,7 +5,9 @@ export function rnd(n) {
 function setCookie(name, value, daysToExpire = 30, path = "/") {
   const date = new Date();
   date.setTime(date.getTime() + daysToExpire * 24 * 60 * 60 * 1000); // Convert days to milliseconds
-  const cookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)}; expires=${date.toUTCString()}; path=${path}`;
+  const cookie = `${encodeURIComponent(name)}=${encodeURIComponent(
+    value
+  )}; expires=${date.toUTCString()}; path=${path}`;
   document.cookie = cookie;
 }
 
@@ -18,4 +20,13 @@ function getCookie(name) {
     }
   }
   return null;
+}
+
+export function setVol(vol) {
+  setCookie("volume", vol);
+}
+
+export function getVol() {
+  let vol = getCookie("volume") ? getCookie("volume") : 100;
+  return vol;
 }
