5ccf0680d5a725aec5636f3ca142e1662cef30b0
shabane <m.mohamadshabane@gmail.com>
2025-01-26 02:48:03 +0330

    feat: fn to set cookie

--- src/tools.js
+++ src/tools.js
@@ -1,3 +1,10 @@
 export function rnd(n) {
   return Math.floor(Math.random() * n);
 }
+
+function setCookie(name, value, daysToExpire = 30, path = "/") {
+  const date = new Date();
+  date.setTime(date.getTime() + daysToExpire * 24 * 60 * 60 * 1000); // Convert days to milliseconds
+  const cookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)}; expires=${date.toUTCString()}; path=${path}`;
+  document.cookie = cookie;
+}
