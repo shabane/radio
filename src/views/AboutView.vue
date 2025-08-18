dd7b64e3342d0a99296ee8515e95b4203ec8513a
shabane <m.mohamadshabane@gmail.com>
2025-08-18 20:47:31 +0330

    maintain: write about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,64 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about container mb-5">
+    <div class="mt-5 rounder my-box p-5">
+      <h1>Why?</h1>
+      <hr class="m-5" size="1px" color="#606060" />
+      <p class="">
+        I write this site cause that i have some songs that i love to listen
+        again, but i live in hell Iran which internet is too much slow and
+        censored by governshit! so i have my songs offline(spotify have this
+        feature? yes but hell i should by its account which is expensive to
+        iraninans) so far so good, then i need my song everywherek, at home and
+        work, but it was the time that i realize that my phone have not enough
+        space to carry that much song. when i was thinking about how to code
+        this, i thouth that how can i make it severless and free at least!? i
+        used github pages to deploy this code which wroted with VueJs, and i put
+        the songs at github repos too!
+      </p>
+    </div>
+
+    <div class="mt-5 rounder my-box p-5">
+      <h1>Who?</h1>
+      <hr class="m-5" size="1px" color="#606060" />
+      <p>
+        Im Arya, i work on computer stuffs that are cool for me. you can find my
+        posts on my
+        <a href="https://wiregeek.ir">Blog</a>
+        or you can reach me at my
+        <a href="mailto:m.mohamadshanane@gmail.com">Mail</a> and
+        <a href="https://x.com/m_shabane">Twitter</a>
+      </p>
+    </div>
+
+    <div class="mt-5 rounder my-box p-5">
+      <h1>How?</h1>
+      <hr class="m-5" size="1px" color="#606060" />
+      <p>
+        If you wonder how to put your songs here, i shoud say that is too easy,
+        just fork this
+        <a href="https://github.com/js-radio/station_schema">repo</a>
+        then put your songs in
+        <code>files/musics/</code>
+        then just leave a issue
+        <a href="https://github.com/shabane/radio/">here</a>. just with this
+        steps you have your own station :)
+      </p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.rounder {
+  border-radius: 10px;
+}
+
+.my-box {
+  background: #313131;
+  color: bisque;
+}
+
+a {
+  color: #bdb5d5;
+  text-decoration: none;
+}
+</style>
