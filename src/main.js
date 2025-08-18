c1547b3d0b399bd00292e0185d289c453fbf99af
shabane <m.mohamadshabane@gmail.com>
2025-08-18 16:41:18 +0330

    maintain: use bundle of bootstrap

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import "./assets/style.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 let Vue = createApp(App);
 Vue.use(router);
